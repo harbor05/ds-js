d9e233f4d9833a117cb10570c5dad5881c5b707a
Joowon Jang <joowon.jang@hotmail.com>
2022-05-11 23:48:06 +0900

    Init

---
+++ src/Stack/ArrayStack.js
@@ -0,0 +1,83 @@
+/**
+ * Array Stack
+ * @description 배열의 인덱스를 활용한 배열 스택
+ * Pop, Push, Peek 구현
+ * 메모리 문제를 해결하기 위해, 가변 배열 방식이나, 리스트 스택을 활용
+ */
+class ArrayStack {
+  top = -1; // 배열의 타겟 Index, 최소 값은 -1, 최대 값이 배열의 길이
+
+  constructor(stackSize) {
+    // Stack Size에 해당하는 배열 생성
+    this.stack = new Array(stackSize);
+  }
+
+  // 데이터가 없는 경우 확인
+  isEmpty() {
+    return this.top === -1;
+  }
+
+  // 스택이 모두 채워 있는지 확인
+  isFull() {
+    return this.top === this.stack.length - 1;
+  }
+
+  // 데이터를 넣을 때
+  push(data) {
+    if (this.isFull()) {
+      throw new Error("Stack is Full");
+    } else {
+      // top을 1 증가 시키고 data를 넣는다.
+      this.stack[++this.top] = data;
+    }
+  }
+
+  // 데이터를 꺼낼 때
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is Empty");
+    } else {
+      // 데이터를 꺼낸다.
+      let tempData = this.stack[this.top];
+      // 데이터를 꺼낸 곳에 null을 저장하고 top을 1을 감소 시킨다.
+      this.stack[this.top--] = null;
+      return tempData;
+    }
+  }
+
+  // 데이터 확인
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      return this.stack[this.top];
+    }
+  }
+}
+
+// Stack Size를 넣어주기
+const arrayStack = new ArrayStack(4);
+
+arrayStack.push("a");
+arrayStack.push("b");
+arrayStack.push("c");
+arrayStack.push("d");
+
+console.log("Start Pushed", arrayStack);
+
+console.log("Pop1 Data", arrayStack.pop());
+console.log("pop1", arrayStack);
+
+console.log("Pop2 Data", arrayStack.pop());
+console.log("pop2", arrayStack);
+
+console.log("Pop3 Data", arrayStack.pop());
+console.log("pop3", arrayStack);
+
+arrayStack.push("e");
+
+console.log("Push e", arrayStack);
+
+console.log("Peek Data", arrayStack.peek());
+
+console.log("Final", arrayStack);
---
+++ src/Stack/ListStack.js
@@ -0,0 +1,85 @@
+// Linked List Stack
+
+class Node {}
+
+/**
+ * List Stack
+ * @description 연결 리스트를 활용한 스택
+ * Pop, Push, Peek 구현
+ * Head가 null 인 경우에 예외처리 필요
+ */
+class ListStack {
+  head; // head
+
+  constructor() {}
+
+  // 데이터가 없는 경우 확인
+  isEmpty() {
+    return this.head === null;
+  }
+
+  // 데이터를 넣을 때
+  push(data) {
+    // 새로운 노드를 만든다.
+    let node = new Node();
+    // 생성된 노드에 데이터 주입
+    node.data = data;
+    // Head(앞선 노드)가 비어있지 않으면
+    if (!this.isEmpty()) {
+      node.next = this.head;
+    }
+    // Head는 새로운 노드의 값을 저장
+    this.head = node;
+  }
+
+  // 데이터를 꺼낼 때
+  pop() {
+    if (this.isEmpty()) {
+      // head가 null인 경우 데이터가 없음
+      throw new Error("List Stack is Empty");
+    } else {
+      let targetNode = this.head;
+      // 노드의 데이터를 임시저장
+      let tempData = targetNode.data;
+      // 삭제할 노드 다음 노드의 참조값을 저장
+      this.head = targetNode.next;
+      // 삭제할 노드의 연결을 끊는다.
+      targetNode.data = null;
+      targetNode.next = null;
+
+      return tempData;
+    }
+  }
+
+  // 데이터 확인
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      return this.head.data;
+    }
+  }
+}
+
+const listStack = new ListStack();
+
+listStack.push("a");
+listStack.push("b");
+listStack.push("c");
+listStack.push("d");
+
+console.log("Pop1 Data", listStack.pop());
+console.log("pop1", listStack);
+
+console.log("Pop2 Data", listStack.pop());
+console.log("pop2", listStack);
+
+console.log("Pop3 Data", listStack.pop());
+console.log("pop3", listStack);
+
+listStack.push("e");
+console.log("Push e", listStack);
+
+console.log("Peek Data", listStack.peek());
+
+console.log("Final", listStack);
